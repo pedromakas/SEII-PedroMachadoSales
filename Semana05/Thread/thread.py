3ce6884325ea08f2fe1fed4b06e81d80794cdb30
pedromakas <82834883+pedromakas@users.noreply.github.com>
2022-11-02 20:59:34 -0300

    Create thread.py

---
+++ Semana05/Thread/thread.py
@@ -0,0 +1,55 @@
+import requests
+import time
+import concurrent.futures
+
+img_urls = []
+
+t1 = time.perf_counter()
+
+
+def download_image(img_url):
+    img_bytes = requests.get(img_url).content
+    img_name = img_url.split('/')[3]
+    img_name = f'{img_name}.jpg'
+    with open(img_name, 'wb') as img_file:
+        img_file.write(img_bytes)
+        print(f'{img_name} was downloaded...')
+
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    executor.map(download_image, img_urls)
+
+
+t2 = time.perf_counter()
+
+print(f'Finished in {t2-t1} seconds')
+
+start = time.perf_counter()
+
+
+def do_something(seconds):
+    print(f'Sleeping {seconds} second(s)...')
+    time.sleep(seconds)
+    return f'Done Sleeping...{seconds}'
+
+
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    secs = [5, 4, 3, 2, 1]
+    results = executor.map(do_something, secs)
+
+    for result in results:
+        print(result)
+
+threads = []
+
+for _ in range(10):
+    t = threading.Thread(target=do_something, args=[1.5])
+    t.start()
+    threads.append(t)
+
+for thread in threads:
+    thread.join()
+
+finish = time.perf_counter()
+
+print(f'Finished in {round(finish-start, 2)} second(s)')
