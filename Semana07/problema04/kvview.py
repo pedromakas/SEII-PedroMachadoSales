a1a5a37c931b2a52dff082f14a88dcb4bef535e9
pedromakas <82834883+pedromakas@users.noreply.github.com>
2022-11-20 21:09:10 -0300

    Add files via upload

---
+++ Semana07/problema04/kvview.py
@@ -0,0 +1,19 @@
+# importing widgets
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+# The App class dictates the window name
+
+
+class whats(App):
+    def build(self):
+        pass
+
+
+# run Say Hello App Calss
+if __name__ == "__main__":
+    whats().run()
